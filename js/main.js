7460a575280598a7c246bd2d5bd27fa3a03979d6
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-06-15 21:01:01 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,59 @@
+ // ------- 1-ый пункт задания --------
+var avatar = ['img/avatars/user01.png','img/avatars/user02.png','img/avatars/user03.png',
+  'img/avatars/user04.png','img/avatars/user05.png','img/avatars/user06.png','img/avatars/user07.png',
+  'img/avatars/user08.png'];
+var type = ['palace', 'flat', 'house', 'bungalo'];
+var x = [];
+var y = [];
+
+var ads = [
+  {
+    "author": avatar[3] ,
+    "offer": type[3],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[0] ,
+    "offer": type[2],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[1] ,
+    "offer": type[1],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[5] ,
+    "offer": type[0],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[2] ,
+    "offer": type[0],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[4] ,
+    "offer": type[1],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[6] ,
+    "offer": type[2],
+    "location": (x[], y[])
+  },
+  {
+    "author": avatar[7] ,
+    "offer": type[3],
+    "location": (x[], y[])
+  }
+];
+
+
+// ------- 2-ой пункт задания --------
+var map = document.querySelector('.map');
+
+map.classList.remove('map--faded');
+
+
+// ------- 3-ий пункт задания --------
