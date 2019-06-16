bbbd4222e68c41929141dddc752aa0a3031139f1
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-06-16 21:55:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,49 +3,49 @@ var avatar = ['img/avatars/user01.png','img/avatars/user02.png','img/avatars/use
   'img/avatars/user04.png','img/avatars/user05.png','img/avatars/user06.png','img/avatars/user07.png',
   'img/avatars/user08.png'];
 var type = ['palace', 'flat', 'house', 'bungalo'];
-var x = [];
-var y = [];
+var x = [200, 250];
+var y = [300, 350];
 
 var ads = [
   {
-    "author": avatar[3] ,
-    "offer": type[3],
-    "location": (x[], y[])
+    author: avatar[3] ,
+    offer: type[3],
+    location: (x[1], y[0])
   },
   {
-    "author": avatar[0] ,
-    "offer": type[2],
-    "location": (x[], y[])
+    author: avatar[0] ,
+    offer: type[2],
+    // location: x[], y[]
   },
   {
-    "author": avatar[1] ,
-    "offer": type[1],
-    "location": (x[], y[])
+    author: avatar[1] ,
+    offer: type[1],
+    // location: x[], y[]
   },
   {
-    "author": avatar[5] ,
-    "offer": type[0],
-    "location": (x[], y[])
+    author: avatar[5] ,
+    offer: type[0],
+    // location: x[], y[]
   },
   {
-    "author": avatar[2] ,
-    "offer": type[0],
-    "location": (x[], y[])
+    author: avatar[2] ,
+    offer: type[0],
+    // location: x[], y[]
   },
   {
-    "author": avatar[4] ,
-    "offer": type[1],
-    "location": (x[], y[])
+    author: avatar[4] ,
+    offer: type[1],
+    // location: x[], y[]
   },
   {
-    "author": avatar[6] ,
-    "offer": type[2],
-    "location": (x[], y[])
+    author: avatar[6] ,
+    offer: type[2],
+    // location: x[], y[]
   },
   {
-    "author": avatar[7] ,
-    "offer": type[3],
-    "location": (x[], y[])
+    author: avatar[7] ,
+    offer: type[3],
+    // location: x[], y[]
   }
 ];
 
@@ -57,3 +57,20 @@ map.classList.remove('map--faded');
 
 
 // ------- 3-ий пункт задания --------
+
+var mapPointList = document.querySelector('.map__pins');
+var mapPoint = mapPointList.querySelector('.map__pin');
+
+var renderPoints = function (ad) {
+  var pointElement = mapPoint.cloneNode(true);
+
+  pointElement.querySelector('img') = ad.author;
+
+  return pointElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < ads.length; i++) {
+  fragment.appendChild(renderPoints(ads[i]));
+}
+mapPointList.appendChild(fragment);
