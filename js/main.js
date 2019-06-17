77d08def404b53e0c803a6fc97a4d71d73d8a30a
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-06-17 23:14:05 +0300

    задание 3

--- js/main.js
+++ js/main.js
@@ -1,76 +1,67 @@
- // ------- 1-ый пункт задания --------
-var avatar = ['img/avatars/user01.png','img/avatars/user02.png','img/avatars/user03.png',
+var avatars = ['img/avatars/user01.png','img/avatars/user02.png','img/avatars/user03.png',
   'img/avatars/user04.png','img/avatars/user05.png','img/avatars/user06.png','img/avatars/user07.png',
   'img/avatars/user08.png'];
-var type = ['palace', 'flat', 'house', 'bungalo'];
-var x = [200, 250];
-var y = [300, 350];
-
-var ads = [
-  {
-    author: avatar[3] ,
-    offer: type[3],
-    location: (x[1], y[0])
-  },
-  {
-    author: avatar[0] ,
-    offer: type[2],
-    // location: x[], y[]
-  },
-  {
-    author: avatar[1] ,
-    offer: type[1],
-    // location: x[], y[]
-  },
-  {
-    author: avatar[5] ,
-    offer: type[0],
-    // location: x[], y[]
-  },
-  {
-    author: avatar[2] ,
-    offer: type[0],
-    // location: x[], y[]
-  },
-  {
-    author: avatar[4] ,
-    offer: type[1],
-    // location: x[], y[]
-  },
-  {
-    author: avatar[6] ,
-    offer: type[2],
-    // location: x[], y[]
-  },
-  {
-    author: avatar[7] ,
-    offer: type[3],
-    // location: x[], y[]
-  }
-];
-
-
-// ------- 2-ой пункт задания --------
+var types = ['palace', 'flat', 'house', 'bungalo'];
+var mapElement = document.querySelector('.map__pins');
+var x = [0, mapElement.offsetWidth];
+var y = [130, 630];
 var map = document.querySelector('.map');
+var mapPoint = mapElement.querySelector('.map__pin');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 
 map.classList.remove('map--faded');
 
+var createPins = function(count) {
+  var mas = [];
 
-// ------- 3-ий пункт задания --------
+  for (var i = 0; i < 8; i++) {
+    var item = {
+      author: avatars[getRandomNumber([0, avatars.length - 1])],
+      offer: {
+        title: 'Заголовок объявления',
+        type: types[getRandomNumber([0, types.length - 1])]
+      },
+      location: {
+        x: getRandomNumber(x),
+        y: getRandomNumber(y)
+      }
+    }
 
-var mapPointList = document.querySelector('.map__pins');
-var mapPoint = mapPointList.querySelector('.map__pin');
+    mas.push(item);
+  }
 
-var renderPoints = function (ad) {
-  var pointElement = mapPoint.cloneNode(true);
-
-  pointElement.querySelector('img') = ad.author;
-
-  return pointElement;
+  return mas;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < ads.length; i++) {
-  fragment.appendChild(renderPoints(ads[i]));
-}
-mapPointList.appendChild(fragment);
+var createElementPin = function(newElement) {
+  var pin = pinTemplate.cloneNode(true);
+  pin.style.left = newElement.location.x - PIN_WIDTH / 2 + 'px';
+  pin.style.top = newElement.location.y - PIN_HEIGHT + 'px';
+  pin.querySelector('img').src = newElement.author;
+  pin.querySelector('img').alt = newElement.offer.title;
+
+  return pin;
+};
+
+var renderPins = function(pins) {
+  var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < pins.length; i++) {
+  var newElement = createElementPin(pins[i]);
+
+  fragment.appendChild(newElement);
+};
+
+mapElement.appendChild(fragment);
+};
+
+var getRandomNumber = function(coordinate) {
+  var num = Math.floor(coordinate[0] + Math.random() * (coordinate[1] + 1 - coordinate[0]));
+
+  return num;
+};
+
+var pins = createPins(8);
+renderPins(pins);
