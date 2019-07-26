44cf15f25479f115b6d0d1839a324e105eefd915
Shujinko53 <shujinko53@yandex.ru>
2019-07-26 23:40:33 +0300

    add display start coords in the address input,  accordance of roomLimits and visitors and remake accordance timeIn and timeOut

--- js/form.js
+++ js/form.js
@@ -3,6 +3,13 @@
 (function () {
   var adForm = document.querySelector('.ad-form');
   var adFormGroups = adForm.querySelectorAll('fieldset');
+  var address = document.querySelector('#address');
+
+  var addPinCoords = function () {
+    address.value = window.pin.getCoords(window.set.mapPoint);
+  };
+
+  addPinCoords();
 
   window.autoFill = function () {
     var x = document.getElementById('type').value;
@@ -22,32 +29,58 @@
     }
   };
 
-  window.autoFillTime = function () {
-    var timeIn = document.getElementById('timein').value;
-    var timeOut = document.getElementById('timeout');
+  /* Соответствие комнат и мест */
+  var roomLimits = {
+    1: ['1'],
+    2: ['1', '2'],
+    3: ['1', '2', '3'],
+    100: ['0']
+  };
 
-    if (timeIn === '12:00') {
-      timeOut.value = '12:00';
-    } else if (timeIn === '13:00') {
-      timeOut.value = '13:00';
-    } else if (timeIn === '14:00') {
-      timeOut.value = '14:00';
+  var roomOptions;
+  var roomNumber = document.querySelector('#room_number');
+  var capacity = document.querySelector('#capacity');
+  var capacityOptions = document.querySelectorAll('#capacity option');
+
+  var roomNumberChangeHandler = function () {
+    roomOptions = roomLimits[roomNumber.value];
+    capacityOptions.forEach(function (elem) {
+      if (roomOptions.indexOf(elem.value) === -1) {
+        elem.setAttribute('hidden', '');
+      } else {
+        elem.removeAttribute('hidden');
+      }
+    });
+    capacityChangeHandler();
+  };
+
+  var capacityChangeHandler = function () {
+    roomOptions = roomLimits[roomNumber.value];
+    if (roomOptions.indexOf(capacity.value) === -1) {
+      capacity.setCustomValidity('Число гостей не соответствует ограничениям для данного числа комнат!');
+    } else {
+      capacity.setCustomValidity('');
     }
   };
 
-  window.autoFillOutTime = function () {
-    var timeOut = document.getElementById('timeout').value;
-    var timeIn = document.getElementById('timein');
+  roomNumber.addEventListener('change', roomNumberChangeHandler);
+  capacity.addEventListener('change', capacityChangeHandler);
 
-    if (timeOut === '12:00') {
-      timeIn.value = '12:00';
-    } else if (timeOut === '13:00') {
-      timeIn.value = '13:00';
-    } else if (timeOut === '14:00') {
-      timeIn.value = '14:00';
-    }
+  /* Соответствие времени въезда и выезда */
+  var timeIn = document.querySelector('#timein');
+  var timeOut = document.querySelector('#timeout');
+  var syncValues = function (field1, field2) {
+    field1.value = field2.value;
   };
 
+  timeIn.addEventListener('change', function () {
+    syncValues(timeOut, timeIn);
+  });
+  timeOut.addEventListener('change', function () {
+    syncValues(timeIn, timeOut);
+  });
+
+  /* Сброс формы по кнопке reset*/
   window.set.formReset.addEventListener('click', function () {
     window.set.map.classList.add('map--faded');
     window.set.form.classList.add('ad-form--disabled');
@@ -56,7 +89,20 @@
     window.pin.removePins();
     window.set.mapPoint.style.left = window.set.initLeft;
     window.set.mapPoint.style.top = window.set.initTop;
+
     window.set.setAttributeAll(adFormGroups, 'disabled');
+    window.set.filtersSelects.forEach(function (elem) {
+      elem.value = 'any';
+    });
+    window.set.filterFeatures.forEach(function (elem) {
+      elem.checked = false;
+    });
+    window.set.form.reset();
+    addPinCoords();
   });
+
+  window.form = {
+    roomNumberChangeHandler: roomNumberChangeHandler
+  };
 
 })();
