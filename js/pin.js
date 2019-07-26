d09da60dd8b283b13ceeb54904c617fd5b1e8534
Shujinko53 <shujinko53@yandex.ru>
2019-07-26 23:45:13 +0300

    add function of get the coords of the pin

--- js/pin.js
+++ js/pin.js
@@ -69,10 +69,19 @@
     }
   };
 
+
   window.pin = {
     renderPins: renderPins,
     removePins: removePins,
-    deleteActiveClass: deleteActiveClass
+    deleteActiveClass: deleteActiveClass,
+
+    /* Получение координат элемента */
+    getCoords: function (element, posY) {
+      var coordX = Math.floor(parseInt(element.style.left, 10) + element.clientWidth / 2);
+      var y = element.clientHeight;
+      var coordY = Math.floor(parseInt(element.style.top, 10) + (posY === 'bottom' ? y : y / 2));
+      return coordX + ', ' + coordY;
+    }
   };
 
 })();
